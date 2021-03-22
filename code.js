ddcf95b7720a14ce0bed95dc59226ecab350c589
sarcan <73133257+sarcan@users.noreply.github.com>
2021-03-22 19:16:27 +0100

    update code.js

--- code.js
+++ code.js
@@ -4,19 +4,22 @@ var requestOptions = {
   };
   
   fetch("https://api.nasa.gov/planetary/apod?api_key=lfgYSAttcNLMTq4Z04eeVldbx3PK83pSrufWlYFA", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(data => {
 
+      data.forEach(object => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+      
+        const template = `
+        <img class='image'> ${object.url} </img>
+        <p class='title'> ${object.title} </p>
+        <p class='date'> ${object.date} </p>
+        `
+      
+        card.innerHTML = template
+        document.querySelector('#container').appendChild(card)
 
-    const card = document.createElement('div')
-    card.classList.add('card')
-  
-    const template = `
-    <img class='image'> ${result.url} </img>
-    <p class='title'> ${result.title} </p>
-    <p class='date'> ${result.date} </p>
-    `
-  
-    card.innerHTML = template
-    document.querySelector('.container').appendChild(card)
+      });
+    })
