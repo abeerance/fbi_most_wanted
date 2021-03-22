e17504ae44dbdc806e153cdab0420a89044c4917
abeerance <hadrian.chio@gmail.com>
2021-03-22 19:40:24 +0100

    minor

--- code.js
+++ code.js
@@ -1,27 +1,25 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.nasa.gov/planetary/apod?api_key=lfgYSAttcNLMTq4Z04eeVldbx3PK83pSrufWlYFA", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .then(data => {
+async function fetchData() {
+  var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+  const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=lfgYSAttcNLMTq4Z04eeVldbx3PK83pSrufWlYFA", requestOptions)
+  const data = await response.json()
+  data.forEach(obj => {
+    const card = document.createElement('div')
+    card.classList.add('card')
 
-      result.forEach(element => {
-        const card = document.createElement('div')
-        card.classList.add('card')
-      
-        const template = `
-        <img class='image'> ${element.url} </img>
-        <p class='title'> ${element.title} </p>
-        <p class='date'> ${element.date} </p>
-        `
-      
-        card.innerHTML = template
-        document.querySelector('#container').appendChild(card)
+    const template = `
+    <img class='image'> ${element.url} </img>
+    <p class='title'> ${element.title} </p>
+    <p class='date'> ${element.date} </p>
+    `
 
-      });
-    })
-  .catch(error => console.log('error', error));
+    card.innerHTML = template
+    document.querySelector('#container').appendChild(card)
+  })
+}
+
+
     
