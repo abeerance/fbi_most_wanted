7f9b8b4afa6cbc11ca7040a5aff6535b7a5b81c8
abeerance <hadrian.chio@gmail.com>
2021-03-22 20:09:36 +0100

    last changes

--- queries.js
+++ queries.js
@@ -4,6 +4,44 @@ $(document).ready(function () {
     let countPictures = undefined;
     let getURL = undefined;
   
+    document.querySelector('button').addEventListener('click', imageToGenerate)
+
+    function imageToGenerate(e){
+        const summImg = document.querySelector('#counter').value
+
+        if (summImg.length > 0){
+            if (!isNaN(summImg)){
+                document.querySelector('#container').innerHTML=''
+            getURL = "https://api.nasa.gov/planetary/apod?api_key=lfgYSAttcNLMTq4Z04eeVldbx3PK83pSrufWlYFA&count=" + summImg
+            fetch(getURL, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(element => {
+                    // create cards
+                    const card = document.createElement('div')
+                    card.classList.add('card')
+
+                    const template = `
+                    <img class='image' src="${element.url}">
+                    <p class='title'> ${element.title} </p>
+                    <p class='date'> ${element.date} </p>
+                    `
+                    // append the template
+                    card.innerHTML = template
+                    document.querySelector('#container').appendChild(card)
+                });
+            })
+            console.log(getURL)
+            }
+            else{
+                console.log('Input is not a number')
+            }
+        }
+        else{
+            console.log('you must write a number in the box')
+        }
+    }
+
     button.click(function(e) {
 
         countPictures = document.querySelector('#counter').value
@@ -11,9 +49,7 @@ $(document).ready(function () {
             console.log('not a number')
         }
         else{
-            getURL = "https://api.nasa.gov/planetary/apod?count=" + countPictures + "&api_key=4A0czHEIXr9B9SP73MUqDgAnne4PjzKWAj1k7NeZ"
-            fetch(getURL, requestOptions)
-            console.log(getURL)
+            
         }
     })
 })
